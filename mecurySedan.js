9e72be088b2e939319742ad0512ea671b5093ad9
Timothy Carter <talic2002@gmail.com>
2021-12-20 17:06:17 -0500

    First submission of Automotive Array activity.

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,64 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+
+import Vehicle from "./vehicle.js"
+
+const VehicleModule = require("./vehicleBaseClass.js")
+
+
+class Model extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+    super(make, model, year, color, mileage)
+    this.maximumPassengers = 6;
+    this.passengers = 0;
+    this.numberOfWheels = 4;
+    this.maximumSpeed = 200;
+    this.fuel = 100;
+    this.scheduleService = false;
+    }
+
+    loadPassenger(num){
+        if (this.passenger < this.maximumPassengers) {
+            if((num + this.passenger) <= this.maximumPassengers)
+                this.passenger = num;
+                return this.passenger;
+            } else {
+                console.log(this.model + " " + this.make + " not enough room for all passengers.");
+            
+        } else { 
+            console.log(this.model + " " + this.make + " is full.");
+        }
+    }
+
+
+    }
+    start() {
+        if(this.fuel > 0) {
+        console.log("Engine has started.")
+        
+        } else {
+        console.log("Out of fuel.");
+        }
+    }
+    
+    scheduleService(mileage) {
+        if(this.mileage > 30000) {
+            this.scheduleService == true;
+            return this.scheduleService
+        
+        }
+        
+        
+    }
+
+
+    
+
+    
+     
+
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+
--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -76,7 +76,7 @@ class Vehicle {
         if (this.numberOfWheels == 8 && 8 == wheels) {
             console.log(this.model + " " + this.make + " is a Truck");
         } else if (this.numberOfWheels == 4 && 4 == wheels) {
-            console.log(this.model + " " + this.make + " is a CAr");
+            console.log(this.model + " " + this.make + " is a Car");
         } else if (this.numberOfWheels == 2 && 2 == wheels) {
             console.log(this.model + " " + this.make + " is a Bike");
         } else {
